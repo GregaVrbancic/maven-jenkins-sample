05cd32112b4315267f76b24dceabe70f361d3ef9
Grega Vrbančič <grega.vrbancic@gmail.com>
2016-03-02 17:00:16 +0100

    add test method.

--- src/test/java/xyz/grega/mavenjenkinssample/TestBankAccount.java
+++ src/test/java/xyz/grega/mavenjenkinssample/TestBankAccount.java
@@ -1,6 +1,15 @@
 package xyz.grega.mavenjenkinssample;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class TestBankAccount {
 
+    @Test
+    public void testDebitWithSufficientFunds() {
+        BankAccount ba = new BankAccount(10);
+        double amount = ba.debit(5);
+        Assert.assertEquals(5.0, amount);
+    }
 
 }
